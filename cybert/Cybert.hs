53255057c9a07fb753863d2001f09ebe37ff8742
darlliu <darlliu@lavabit.com>
2013-05-25 00:33:46 -0700

    should work now, exercise considered done?

--- cybert/Cybert.hs
+++ cybert/Cybert.hs
@@ -13,7 +13,8 @@
 module Cybert
 (
     Cybert_entry(NA, Cybert,probe,genesym,
-    sample, collection, mean,pval,ratio,secondaryRefs,secondaryData),
+        sample, collection, mean, bf, bh, sds, 
+        pval,ratio,secondaryRefs,secondaryData),
     cybert_entry,
     showCybertEntries,
     entriesByFold,
@@ -30,9 +31,10 @@ module Cybert
 {-end module definitions -}
 import Data.Bits
 import Data.List
+import Data.Maybe
+import Data.Char
 import System.IO
 import System.IO.Error
-import Data.Maybe
 import qualified Data.Set as S
 import qualified Data.Map as M
 import qualified Data.ByteString.Char8 as B
@@ -44,7 +46,10 @@ data Cybert_entry = NA | Cybert {
     sample :: [String] ,
     collection :: Maybe String , --which dataset
     mean :: Either Float [Float] , --multiple means
+    sds :: Either Float [Float],
     pval :: Float, --the pairwise or ANOVA pval
+    bf :: Float,
+    bh :: Float,
     ratio :: Either Float [Float], --pairwise ratio or numerous ratios
     secondaryRefs :: M.Map String String , --optional secondary refs
     secondaryData :: M.Map String Float , --optional secondary data
@@ -57,7 +62,10 @@ cybert_entry = Cybert{
     sample = [],
     collection = Nothing,
     mean = Right [],
+    sds = Right [],
     pval = -1,
+    bf = -1,
+    bh = -1,
     ratio = Left (-1),
     secondaryRefs = M.fromList [("","")],
     secondaryData = M.fromList [("",0)],
@@ -95,8 +103,8 @@ instance Show Cybert_entry where
     showsPrec _ a s = show a ++ s
     show NA = "NA"
     show Cybert {probe=p, genesym=sym, sample=ss, collection=col, mean=m,
-    pval=pv, ratio=r, secondaryRefs=_, secondaryData=_,raw=_} = p++"\t"++(showJustS sym)++"\t"
-        ++(showEitherF m)++"\t"++(show pv)++"\t"++(showJustS col)
+    pval=pv, ratio=r, secondaryRefs=_, secondaryData=_,raw=_} = p++"\t"++
+        (showJustS sym)++"\t"++(showJustS col) ++"\t" ++(showEitherF m)++"\t"++(show pv)
         -- for each sample
 {-end format and show routines-}
 {-set operation and filtering routines-}
@@ -131,19 +139,55 @@ cybertToSet xs = S.fromList xs
 --set operations
 {-end set operation and filtering routines-}
 {-IO routines-}
-buildHeader :: B.ByteString -> Maybe (M.Map String Int)
-buildHeader s = let ss = B.split '\t' s
-                in M.fromList (foldl pred [] ss) where
-                   pred acc x = let xx = B.unpack x
-                                in case  xx of "meanC" -> acc : ("meanc", idx)
-                                               "meanE" -> acc : ("meane", idx)
-                                               "meanc" -> acc : ("meanc", idx)
-                                               "meane" -> acc : ("meanc", idx)
+buildHeaderPrec :: B.ByteString -> [(String , B.ByteString)]
+buildHeaderPrec s = let ss = B.split '\t' s
+                in map (\x -> (stripQuote $ B.unpack  x, x)) ss where
+                   stripQuote ('\"':xs) = map toLower $ take ((length xs) - 1) xs
+                   stripQuote xs = map toLower xs
 
+buildHeader :: B.ByteString -> (M.Map String (Maybe Int) )
+buildHeader s = let ss = buildHeaderPrec s; sl = B.split '\t' s
+                in M.fromList $ map  (\x -> (fst x , (snd x) `elemIndex` sl)) ss
+maybeGet :: [B.ByteString] -> Maybe Int -> B.ByteString
+[] `maybeGet` _ = B.pack ""
+x `maybeGet` Nothing = B.pack ""
+x `maybeGet` (Just s)= if length x > s then x !! s
+                        else B.pack ""
 
-lineToCybert :: (Maybe (M.Map String Int))-> [Cybert_entry] -> B.ByteString -> [Cybert_entry]
+getText :: (M.Map String (Maybe Int))-> [B.ByteString] -> String -> Maybe String
+getText header ss id = if id `M.notMember` header
+                         then Nothing
+                         else let idx = header M.! id
+                              in Just (B.unpack $ ss `maybeGet` idx)
+getNum :: (M.Map String (Maybe Int))-> [B.ByteString] -> String -> Float
+getNum header ss id = if id `M.notMember` header
+                        then -1 :: Float
+                        else let idx = header M.! id
+                             in read (B.unpack $ ss `maybeGet` idx) :: Float
+getNums :: (M.Map String (Maybe Int))-> [B.ByteString] -> [String] -> Either Float [Float]
+getNums header ss ids = let nums = map (getNum header ss) ids
+                         in if length nums == 1 then Left (nums !! 0)
+                              else Right $ filter (not . (== -1)) nums
+lineToCybert :: (M.Map String (Maybe Int))-> [Cybert_entry] -> B.ByteString -> [Cybert_entry]
 --take a header and an accumulator, then read the line and append the cybert entry
-lineToCybert header xs line = xs
+lineToCybert header xs line = xs ++ readLine line where
+    readLine s = let ss = B.split '\t' s 
+                  in if length ss /= M.size header then []
+                     else let cybt= cybert_entry {
+                         probe = B.unpack $ ss `maybeGet` (header M.! "probe_id"),
+                         --this is a must
+                         genesym = getText header ss "gene_sym",
+                         --this is of maybe type
+                         pval = getNum header ss "pval",
+                         bf = getNum header ss "bonferroni",
+                         bh = getNum header ss "bh",
+                         --these are -1 defaulted
+                         mean = getNums header ss $ map ( "mean" ++ ) ["c","e","1","2","3","4","5","6","7"],
+                         sds = getNums header ss $ map ( "std" ++ ) ["c","e","1","2","3","4","5","6","7"]
+                         --these are one or many
+                         , raw = s
+                         --raw info
+                         } in [cybt]
 
 loadCybert :: String -> IO (Maybe [Cybert_entry])
 loadCybert fname = catch
@@ -151,14 +195,15 @@ loadCybert fname = catch
         contents <- B.hGetContents handle
         let mylines =  B.split '\n' contents
         if length mylines <= 1 then return Nothing
-        else let header = buildHeader (head mylines)
-             in if isNothing header then return Nothing
-                else let output = (Just (foldl (lineToCybert header) [] (drop 1 mylines)))
-                     in if output == (Just []) then return Nothing
-                        else return output
+        else let header = buildHeader (head mylines);
+                 output = (Just (foldl (lineToCybert header) [] (drop 1 mylines)))
+             in if output == (Just []) then return Nothing
+                  else do 
+                    putStrLn "Done parsing"
+                    return output
     ))
     (\err -> do
-            if isEOFError err 
+            if isEOFError err
             then do
                  putStrLn "File is empty or truncated."
                  return Nothing
--- cybert/test_cybert.hs
+++ cybert/test_cybert.hs
@@ -1,10 +1,9 @@
 import Cybert
 
 main = do
-    let c = cybert_entry{genesym=(Just "Sym"), mean = (Right [1,2,3])}
-    let d = cybert_entry{genesym=(Just "Sym2"), mean = (Right [1,2,3])}
-    exportCybert [c,d] "tested.txt"
-    exportGeneSyms [c,d] "syms.txt"
-    exportProbes [c,d] "probes.txt"
-    some <- loadCybert "tested.txt"
-    putStrLn (show some)
+    {-
+     -let c = cybert_entry{genesym=(Just "Sym"), mean = (Right [1,2,3])}
+     -let d = cybert_entry{genesym=(Just "Sym2"), mean = (Right [1,2,3])}
+     -}
+    ssome@(Just some)<- loadCybert "test.txt"
+    exportGeneSyms some "tested.txt"
